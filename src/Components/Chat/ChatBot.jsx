aa2bf90e9c689c1334d99b3a0bf19132f7ef735b
Shivraj Kolwankar <120171741+shivraj0124@users.noreply.github.com>
2025-06-17 23:49:20 +0530

    Update ChatBot.jsx

--- src/Components/Chat/ChatBot.jsx
+++ src/Components/Chat/ChatBot.jsx
@@ -38,7 +38,7 @@ export default function Chatbot() {
     setMessages((prev) => [...prev, { sender: "bot", loading: true }]);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/process_data", {
+      const response = await fetch("https://chatbot-service-spta.onrender.com/process_data", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
